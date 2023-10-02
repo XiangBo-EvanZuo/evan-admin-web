85396c4a8426134f668d2d45c6f7e4a5be3979eb
Evan Zuo <739221432@qq.com>
2023-10-03 07:26:41 +0800

    feat: feignGetMenuList

--- src/api/sys/menu.ts
+++ src/api/sys/menu.ts
@@ -2,7 +2,7 @@ import { defHttp } from '/@/utils/http/axios';
 import { getMenuListResultModel } from './model/menuModel';
 
 enum Api {
-    GetMenuList = '/getMenuList',
+    GetMenuList = '/business-resource/user/feignGetMenuList',
 }
 
 /**
@@ -10,5 +10,7 @@ enum Api {
  */
 
 export const getMenuList = () => {
-    return defHttp.get<getMenuListResultModel>({ url: Api.GetMenuList });
+    return defHttp
+        .get<{ list: getMenuListResultModel }>({ url: Api.GetMenuList })
+        .then((res) => res.list);
 };
